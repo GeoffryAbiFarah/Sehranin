f07b1214405b04f7ba6644a3a5a81263901609cb
GeoffryAbiFarah <geoffry_abifarah@hotmail.com>
2021-03-01 22:49:38 +0200

    react router + navigation bar

---
+++ web-front-end/src/components/Home.js
@@ -0,0 +1,15 @@
+import {Component} from 'react';
+import "../styles/Home.css";
+
+class Home extends Component{
+
+    render(){
+        return(
+            <div>
+                Browse for party here
+            </div>
+        );
+    }
+}
+
+export default Home;
