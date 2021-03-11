970fcb9e086bb8a5b22deaaaca9ab591be480424
Geoffry Abi Farah <68099320+GeoffryAbiFarah@users.noreply.github.com>
2021-03-11 22:48:52 +0200

    Delete config.js

--- Backend/config.js
+++
@@ -1,6 +0,0 @@
-// centralize all configuration for the server
-
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb+srv://GeoffryAF:dreamTeam101@sehranin.sre5m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
